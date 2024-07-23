308e3528b6cfe02cb566090f173e25d14cdbd84a
Sijenyi96 <everlynsijenyi96@gmail.com>
2024-07-24 00:41:55 +0300

    update changes

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -126,7 +126,13 @@ document.addEventListener('DOMContentLoaded',function(){
 
        async function fetchQuotesFromServer(){
             try {
-                const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+                const response = await fetch('https://jsonplaceholder.typicode.com/posts',{
+                method: 'POST',
+                  headers:{
+                    'Content-Type': 'application/json'
+                  },
+                  body: JSON.stringify(exampleData)
+                })
                   if (!response.ok){
                     throw new Error('Network response was not ok')
                   }
